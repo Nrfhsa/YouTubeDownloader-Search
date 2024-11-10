53e3fad59c3c8a3cbe66245d098488187c493dcc
Nrfhsa <nurfalahsyufa2@gmail.com>
2024-11-11 00:35:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const archiver = require('archiver');
 const cookiesPath = path.join(__dirname, 'cookies.txt');
 
 // Konfigurasi bot
-const token = "7355536746:AAHWuCdE-HKlcnuc_BiLWNKWQzqbzqDiFAU";
+const token = "MASUKAN TOKEN BOT ANDA YANG DIPEROLEH DARI BOT FATHER";
 const bot = new TelegramBot(token, { polling: true });
 
 // Handler Search
